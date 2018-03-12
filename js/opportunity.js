c29a0740cf4276a90a1516412720b51386a75745
zzpenn <36808320+zzpenn@users.noreply.github.com>
2018-03-11 23:15:32 -0700

    Multiple file uploads. Backup copy of Opportunity.

--- js/opportunity.js
+++ js/opportunity.js
@@ -1,6 +1,74 @@
+$(document).ready(
+    function () {
+        initNewOppForm();
+        $('#formdatetimepicker2').datetimepicker();
+        $('#editOpp').show();
+        $('#newOpp').hide();
+        $('#showNewOpp').click(function (){
+            $('#newOpp').show();
+        });
+
+
+        $('clearFile').click(function() {
+
+        });
+        $('#saveNewOpp').click(function() {
+            saveOpportunity();
+        });
+
+        $('#exitNewOpp').click(function() {
+            $('#newOpp').hide();
+        });
+
+        $('#showEditOpp').hide();  //hiding for 1st pass through
+        $('#showEditOpp').click(function() {
+        });
+    });
+
+
+
+
 function initNewOppForm() {
     getLeadEvals()	;
-};
+}
+
+function uploadAllDocs() {
+    /*
+        var file=$('#uploadFileName')[0].files[0];
+        console.log(file.name);
+        var formData=new FormData();
+        formData.append('file',file,file.name);
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST','http://athena.ecs.csus.edu/~wildcard/php/api/opportunity/update.php')
+        xhr.onload = function() {
+            if(xhr.status == 200) {
+                alert('File uploaded');
+            } else {
+                alert('Error uploading file');
+            }
+        };
+        xhr.send(formData);
+       */
+
+        var numfiles =  $('#uploadMFileName')[0].files.length;
+        var file;
+        var formData=new FormData();
+        for(i=0;i<numfiles;i++){
+            file = $('#uploadMFileName')[0].files[i];
+            formData.append('upload[]', file,file.name);
+            console.log(file.name);
+        }
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST','http://athena.ecs.csus.edu/~wildcard/php/api/opportunity/update.php');
+        xhr.onload = function() {
+            if(xhr.status == 200) {
+                alert('File uploaded');
+            } else {
+            alert('Error uploading file');
+            }
+        };
+        xhr.send(formData);
+}
 
 function getLeadEvals() {
 	var xhr = new XMLHttpRequest();
@@ -13,49 +81,53 @@ function getLeadEvals() {
 				var lead = jsonArray.employee[i];
 				var name = lead.first_name + " " + lead.last_name;
                 $('select').append($('<option>', {value:lead.id, text:name }));
-            };
+            }
 		} else {
             alert("Error response");
 		}
 	};
 	xhr.send();
-};
+}
 
 function saveOpportunity() {
     var scoreFile = $('#criteriaFile')[0].files[0];
-    var opId = $('#formIdInput').val();
+    var opId = parseInt($('#formIdInput').val());
     var name = $('#formNameInput').val();
     var desc = $('#formDescriptionInput').val();
-    var close = $('#formdatetimepicker2').data("DateTimePicker").viewDate().toString()
+    var close = $('#formdatetimepicker2').data("DateTimePicker").viewDate().toString();
     var jsonRecord =
     {"opportunity":
-			{"OpportunityID":"80101",
-				"ClosingDate":"2019-01-01 12:00:00",
+			{"OpportunityID":opId,
+                "Name":name,
+				"ClosingDate":close,
 				"ScoringCategoryBlob":scoreFile,
 				"LeadEvaluatorID":3,
 				"LowestBid":10000,
-				"Description":"This is a great opportunity"}};
+				"Description":desc}};
     var xhr = new XMLHttpRequest();
     xhr.open('POST','http://athena.ecs.csus.edu/~wildcard/php/api/opportunity/create.php',true);
 //    xhr.setRequestHeader('Content-type','application/json; charset=utf-8');  //Creates an  error
     xhr.onload = function() {
         if (xhr.status == 200) {
             alert(xhr.responseText);
+            uploadAllDocs();
         } else {
         	alert("Error saving opportunity");
         }
     };
     xhr.send(jsonRecord);
     console.log("Wrote Json: " + jsonRecord);
-};
+}
 
 function submitDocList() {
 
-};
+}
 
 function getDocList() {
 
-};
+}
+
+
 
 
 
